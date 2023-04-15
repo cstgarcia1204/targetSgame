22be35a211b0f27e2eba46471c58b813c23a5b0a
cstgarcia <ckstgarcia@gmail.com>
2023-04-14 19:17:18 -0700

    modified the html and styles due to calcs of the position of the canvas in the js file

--- main.js
+++ main.js
@@ -1,9 +1,9 @@
-const screen = document.querySelector('canvas');
-	const paint = screen.getContext('2d');		
+    let screen = document.querySelector('canvas');
+	let paint = screen.getContext('2d');		
 	paint.fillStyle = 'lightgrey';
 	paint.fillRect(0,0,600,400); 
 
-	const radio = 10;
+	let radio = 10;
 	let xRandom;
 	let yRandom;
 	let x = 0
@@ -11,7 +11,7 @@ const screen = document.querySelector('canvas');
 	const grithDesign = (x, y, radio, color) => {
 		paint.fillStyle = color;
 		paint.beginPath();
-		paint.arc(x,y,radio,0,2*Math.PI);
+		paint.arc(x, y, radio, 0, 2*Math.PI);
 		paint.fill();
 	}	
 
@@ -21,8 +21,8 @@ const screen = document.querySelector('canvas');
 
 	const targetDesign = (x, y) => {
 		grithDesign(x, y, radio + 20, 'red');
-		grithDesign(x,y,radio + 10, 'white');
-		grithDesign(x,y,radio, 'red');
+		grithDesign(x, y, radio + 10, 'white');
+		grithDesign(x, y, radio, 'red');
 	}
 
 	const randomPosition = (maximum) => {
@@ -38,18 +38,18 @@ const screen = document.querySelector('canvas');
 		x++;
 	}
 	
-	setInterval(refreshScreen, 1500);
+	setInterval(refreshScreen, 1000);
 
 	const shoot = (event) => {
 
-		let x = event.pageX - screen.offsetLeft;
-		let y = event.pageY - screen.offsetTop;
+		let x_event = event.pageX - screen.offsetLeft;
+		let y_event = event.pageY - screen.offsetTop;
 
-		if ((x < xRandom + radio) && 
-			(x > xRandom - radio) &&
-			(y < yRandom + radio) &&
-			(y > yRandom - radio) ) {
-			alert('Successful Shot');
+		if ((x_event < xRandom + radio) && 
+			(x_event > xRandom - radio) &&
+			(y_event < yRandom + radio) &&
+			(y_event > yRandom - radio) ) {
+			alert('Succesful Shot!!!!');
 		}
 	}
 
